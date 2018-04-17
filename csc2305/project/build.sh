f0234ab16d7beefdd29fec1bc48dc648730e2fee
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2018-04-17 15:16:01 -0400

    Minor improvements to build script

--- csc2305/project/build.sh
+++ csc2305/project/build.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+# Experimental script for building this project and its dependencies from scratch.
 
 set -eu
 
+# Make sure you have g++ 5.4 or higher, even on older Ubuntus.
+apt install -y software-properties-common
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y g++-5 gcc-5
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+
+# Ensure an up-to-date CMake is available
 scripts/install_cmake.sh NO_SUDO
 
+# Build Ceres
 (
-    # Build Ceres
     cd third_party
     mkdir -p ceres-bin
     cd ceres-bin
@@ -16,18 +25,11 @@ scripts/install_cmake.sh NO_SUDO
     # No make install, since just making the thing puts it in CMake's registry.
 )
 
-# Make sure you have g++ 5.4 or higher, even on older Ubuntus.
-apt install -y software-properties-common
-add-apt-repository ppa:ubuntu-toolchain-r/test
-apt-get update
-apt-get install -y g++-5 gcc-5
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 90 --slave /usr/bin/g++ g++ /usr/bin/g++-5
-
+# Build the main project
 (
     mkdir build
     cd build
     cmake ..
-
     make -j$(nproc)
 )
 
